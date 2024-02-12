345f456f349bcca4d10b071e86a23bab05c52ec9
Maulana Fadilah <maulanafadilah028@gmail.com>
2024-02-12 11:01:21 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import styles from './style';
+
+import {
+  Navbar,
+  Hero,
+  Stats,
+  Business,
+  Billing,
+  CardDeal,
+  Testimonials,
+  Clients,
+  Cta,
+  Footer
+} from './components';
+
+const App = () => (
+  <div className="w-full overflow-hidden bg-primary">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <Cta />
+        <Footer />
+      </div>
+    </div>
+  </div>
+);
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+import Navbar from './Navbar';
+import Hero from './Hero';
+import Stats from './Stats';
+import Business from './Business';
+import Billing from './Billing';
+import CardDeal from './CardDeal';
+import Testimonials from './Testimonials';
+import Clients from './Clients';
+import Cta from './Cta';
+import Footer from './Footer';
+
+export { Navbar, Billing, CardDeal, Business, Clients, Cta, Stats, Footer, Testimonials, Hero };
